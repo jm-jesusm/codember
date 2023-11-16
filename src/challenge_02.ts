af06c057b404a438c9f8fa4dbb525959da34657e
GosusMeeh <gosusmeeh@gmail.com>
2023-11-16 16:12:28 +0100

    Add: challenge 2 completed

---
+++ src/challenge_02.ts
@@ -0,0 +1,16 @@
+function spyCompiler(input: string) {
+    let commands = input.split('')
+    let cache = 0;
+    let ans = ''
+
+    commands.forEach(command => {
+        if(command == '#') cache++
+        if(command == '@') cache--
+        if(command == '*') cache*=cache
+        if(command == '&') ans = ans.concat(String(cache))
+    })
+
+    return ans
+}
+
+console.log(spyCompiler('&###@&*&###@@##@##&######@@#####@#@#@#@##@@@@@@@@@@@@@@@*&&@@@@@@@@@####@@@@@@@@@#########&#&##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@&'))
